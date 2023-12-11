c0f8aebbca997b9387d2e9a0088a3f3cb95cffc2
chanachai.l <chanachai.limcharone@gmail.com>
2023-12-11 17:10:19 +0700

    :zap: update back card

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,14 +1,17 @@
 let card = document.querySelector(".card");
-let cardid = Math.floor(Math.random() * (78)) + 1;;
-let sound = new Audio("https://astro.meemodel.com/sound/tarotdaily/" + cardid + ".mp3");
+let cardid = Math.floor(Math.random() * 78) + 1;
+let sound = new Audio(
+  "https://astro.meemodel.com/sound/tarotdaily/" + cardid + ".mp3"
+);
 let flip = false;
 
-card.querySelector(".back img").src = "https://astro.meemodel.com/img/tarot/front/" + cardid + ".jpg";
+card.querySelector(".back img").src =
+  "https://astro.meemodel.com/img/tarot/front/" + cardid + ".jpg";
 
 card.addEventListener("click", () => {
   if (!flip) {
     flip = true;
-    card.classList.add('flip');
-    setTimeout(() => (sound.play()), 1000);
+    card.classList.add("flip");
+    setTimeout(() => sound.play(), 1000);
   }
 });
