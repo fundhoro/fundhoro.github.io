b6ffc5230daeeb20e4e2735650c4eb0242480a5d
chanachai.l <chanachai.limcharone@gmail.com>
2023-12-11 17:58:06 +0700

    :zap: correct order of sound and card name king & queen

--- assets/js/core.js
+++ assets/js/core.js
@@ -1,17 +1,37 @@
 let card = document.querySelector(".card");
 let cardid = Math.floor(Math.random() * 78) + 1;
-let sound = new Audio(
-  "https://astro.meemodel.com/sound/tarotdaily/" + cardid + ".mp3"
-);
-let flip = false;
 
-card.querySelector(".back img").src =
-  "https://www.myhora.net/astrology/tarot/card/" + (cardid - 1) + ".png";
+const king = [35, 49, 63, 77];
+const queen = [34, 48, 62, 76];
 
-card.addEventListener("click", () => {
-  if (!flip) {
-    flip = true;
-    card.classList.add("flip");
-    setTimeout(() => sound.play(), 1000);
-  }
-});
+let sound;
+
+function initize(id) {
+  if (king.includes(id - 1))
+    sound = new Audio(
+      "https://astro.meemodel.com/sound/tarotdaily/" + (id - 1) + ".mp3"
+    );
+  else if (queen.includes(id - 1))
+    sound = new Audio(
+      "https://astro.meemodel.com/sound/tarotdaily/" + (id + 1) + ".mp3"
+    );
+  else
+    sound = new Audio(
+      "https://astro.meemodel.com/sound/tarotdaily/" + id + ".mp3"
+    );
+
+  let flip = false;
+
+  card.querySelector(".back img").src =
+    "https://www.myhora.net/astrology/tarot/card/" + (id - 1) + ".png";
+
+  card.addEventListener("click", () => {
+    if (!flip) {
+      flip = true;
+      card.classList.add("flip");
+      setTimeout(() => sound.play(), 1000);
+    }
+  });
+}
+
+initize(cardid);
